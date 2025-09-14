927e54db864383149789981798930b152cbe82be
Илья <redyc123@gmail.com>
2025-09-14 12:51:04 +0500

    add lab 1

--- ATPRV/lab1/Program.cs
+++ ATPRV/lab1/Program.cs
@@ -105,10 +105,10 @@ for (int i = 0; i < all_results.Length; i++)
     Console.WriteLine("{0}. MIN:{1} MAX:{2} MID: {3}", i + 1, min, max, sum / res.Length);
 }
 //1.1
-// 1. MIN:0.208 MAX:0.426 MID: 0.2334099999999999
-// 2. MIN:0.294 MAX:0.46 MID: 0.3386500000000001
-// 3. MIN:0.208 MAX:0.321 MID: 0.22632999999999984
-// 4. MIN:0.297 MAX:0.531 MID: 0.32477999999999996
+// 1. MIN:0,209 MAX:0,315 MID: 0,2177699999999999
+// 2. MIN:0,448 MAX:0,775 MID: 0,5604699999999998
+// 3. MIN:0,188 MAX:0,197 MID: 0,1899800000000001
+// 4. MIN:0,453 MAX:0,768 MID: 0,5670300000000003
 
 //1.2
 // 1. MIN:0.024 MAX:0.071 MID: 0.027409999999999997
@@ -118,10 +118,105 @@ for (int i = 0; i < all_results.Length; i++)
 
 
 // Задание 2
+Console.WriteLine("Задание 2");
 
-double newelem(double aj)
+static double[] ComputeSequential(int[] A)
 {
-    return Math.Pow(aj, 1.789);
+    int n = A.Length;
+    double[] B = new double[n];
+    for (int i = 0; i < n; i++)
+    {
+        double sum = 0;
+        for (int j = 0; j < i; j++)
+        {
+            sum += Math.Pow(A[j], 1.789);
+        }
+        B[i] = sum;
+    }
+    return B;
 }
 
-System.Diagnostics.Process.GetCurrentProcess().Threads;
+static double[] ComputeParallel(int[] A, int numThreads)
+{
+    int n = A.Length;
+    double[] B = new double[n];
+    int chunk = n / numThreads;
+
+    Parallel.For(0, numThreads, k =>
+    {
+        int start = k * chunk;
+        int end = (k == numThreads - 1) ? n : start + chunk;
+
+        for (int i = start; i < end; i++)
+        {
+            double sum = 0;
+            for (int j = 0; j < i; j++)
+            {
+                sum += Math.Pow(A[j], 1.789);
+            }
+            B[i] = sum;
+        }
+    });
+
+    return B;
+}
+
+// Параллельное вычисление с циклическим разбиением
+static double[] ComputeParallelRoundRobin(int[] A, int numThreads)
+{
+    int n = A.Length;
+    double[] B = new double[n];
+
+    Parallel.For(0, numThreads, k =>
+    {
+        for (int i = k; i < n; i += numThreads)
+        {
+            double sum = 0;
+            for (int j = 0; j < i; j++)
+            {
+                sum += Math.Pow(A[j], 1.789);
+            }
+            B[i] = sum;
+        }
+    });
+
+    return B;
+}
+
+
+
+int[] sizes = { 1000, 5000, 10000, 20000 };
+int[] threads = { 1, 2, 4, 8, 12, 16, 20 };
+
+foreach (int n_size in sizes)
+{
+    Console.WriteLine($"\nРазмерность массива: {n_size}");
+    int[] A = new int[n_size];
+    for (int i = 0; i < n_size; i++) A[i] = i + 1;
+
+    foreach (int t in threads)
+    {
+        // Последовательное заполнение
+        DateTime t1 = DateTime.Now;
+        double[] Bseq = ComputeSequential(A);
+        DateTime t2 = DateTime.Now;
+        TimeSpan dt = t2 - t1;
+        // Console.WriteLine($"Потоки: {t}, Последовательно: {dt.Milliseconds / 1000.0} мс");
+
+        // Параллельное заполнение
+        t1 = DateTime.Now;
+        double[] Bpar = ComputeParallel(A, t);
+        t2 = DateTime.Now;
+        dt = t2 - t1;
+        // Console.WriteLine($"Потоки: {t}, Параллельно: {dt.Milliseconds / 1000.0} мс");
+
+         // Параллельное заполнение 2
+        t1 = DateTime.Now;
+        double[] Bpar2 = ComputeParallelRoundRobin(A, t);
+        t2 = DateTime.Now;
+        dt = t2 - t1;
+        Console.WriteLine($"Потоки: {t}, Параллельно 2: {dt.Milliseconds / 1000.0} мс");
+    }
+}
+
+
